31c9339a1275052536cf1acc8fba865430f5514b
scothu <scothu@ROMULUS>
2024-11-10 15:21:54 -0800

    Add project files.

---
+++ AspNetFunction/Function1.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace AspNetFunction
+{
+    public class Function1(ILogger<Function1> logger)
+    {
+        public record InputText(string Value);
+        public record PigLatinText(string Value);
+
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req, [FromBody] InputText inputText)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+            var result = TranslateToPigLatin(inputText.Value);
+            return new OkObjectResult(new PigLatinText(result));
+
+        }
+
+        private static string TranslateToPigLatin(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            var words = input.Split(' ');
+            StringBuilder pigLatin = new();
+
+            foreach (string word in words)
+            {
+                if (IsVowel(word[0]))
+                {
+                    pigLatin.Append(word + "yay ");
+                }
+                else
+                {
+                    int vowelIndex = FindFirstVowelIndex(word);
+                    if (vowelIndex is -1)
+                    {
+                        pigLatin.Append(word + "ay ");
+                    }
+                    else
+                    {
+                        pigLatin.Append(
+                            word.Substring(vowelIndex) + word.Substring(0, vowelIndex) + "ay ");
+                    }
+                }
+            }
+
+            return pigLatin.ToString().Trim();
+        }
+
+        private static int FindFirstVowelIndex(string word)
+        {
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (IsVowel(word[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private static bool IsVowel(char c) => char.ToLower(c) is 'a' or 'e' or 'i' or 'o' or 'u';
+
+    }
+}
